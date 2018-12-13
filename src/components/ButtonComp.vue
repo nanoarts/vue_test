4a3e00d95132270de40b9d6f4d30677adc9dd53d
Jori <nanosix@gmail.com>
2018-12-13 11:50:28 +0100

    this is my commit message

---
+++ src/components/ButtonComp.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- Your HTML goes here -->
+  <ul>
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </ul>
+</template>
+
+<script>
+  /* Your JS goes here */
+  export default {
+  name: 'ButtonComp',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+  /* Your CSS goes here */
+</style>
